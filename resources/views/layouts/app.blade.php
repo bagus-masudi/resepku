6dbda50919842ef8e8d1c102c02053fe6cd1fbef
Ahmad Bagus Mas'udi <ahmadbagusmasudi@gmail.com>
2022-11-19 01:11:03 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+     <!-- Fix style in ngrok -->
+     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   
   <link rel="stylesheet" href="{{asset('bootstrap/dist/css/bootstrap.min.css')}}">
   <link rel="stylesheet" href="{{asset('style/main.css')}}">
